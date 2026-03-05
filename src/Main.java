efe1f5bd4c6587893d16db684baaa49b6aab3741
defol <kostyak294@gmail.com>
2026-03-05 19:22:14 +0300

    add save statistic file

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,45 @@
+// To-do
+// Сделать сохранение статистики игр в файл
+// общее кол-во игр, больше, меньше
+
+
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Random;
+import java.nio.file.Files;
+import java.util.List;
+import java.nio.file.Path;
+
 
 
 public class Main {
-    public static void main(String[] args) {
+    private static final Path FILE_PATH = Path.of("StatisticFile.txt");
+    public static void main(String[] args) throws IOException {
+        InitializationFile();
         Scanner Number = new Scanner(System.in);
 
         System.out.println("Привет в веселой игре\nПопробуй угадать все числа которые я загадаю!");
 
 
-        System.out.println("1.Начать игру\n0.Выйти");
+        System.out.println("1.Начать игру\n2)Статистика\n0.Выйти");
         int menu = Number.nextInt();
 
         switch (menu){
             case 1:
                 Game();
+                break;
+
+            case 2:
+                showStatistics();
+                break;
+
+            case 3:
+                System.out.println("Выход из игры...");
+                break;
         }
 
     }
-    public static void Game(){
+    public static void Game() throws IOException {
         Random randomNumber = new Random();
         Scanner guesserNumber = new Scanner(System.in);
 
@@ -30,12 +51,54 @@ public class Main {
             int guesser = guesserNumber.nextInt();
             if(guesser == number){
                 System.out.println("Поздравляю! Ты угадал");
+                FileSaveStatistic(0);
                 break;
             } else if (guesser < number) {
                 System.out.println("Число которое я загадал больше!");
+                FileSaveStatistic(1);
             }else{
                 System.out.println("Число которое я загадал меньше!");
+                FileSaveStatistic(2);
             }
+        }
+    }
+
+    private static void FileSaveStatistic(int lineIndex) throws IOException{
+        List<String> lines = Files.readAllLines(FILE_PATH);
+        String line = lines.get(lineIndex);
+        int currentValue = Integer.parseInt(line.replaceAll("\\D+", ""));
+        currentValue++;
+        
+        if(lineIndex == 0){
+            lines.set(0, "Выгранно игр: "+ currentValue);
+        } else if (lineIndex == 1) {
+            lines.set(1, "Больше: " + currentValue);
+        }else {
+            lines.set(2, "Меньше: " + currentValue);
+        }
+        Files.write(FILE_PATH, lines);
+    }
+
+    private static void InitializationFile() throws IOException{
+        if (!Files.exists(FILE_PATH)) {
+            Files.write(FILE_PATH, List.of(
+                    "Выиграно игр: 0",
+                    "Больше: 0",
+                    "Меньше: 0"
+            ));
+        }
+    }
+
+    private static void showStatistics() throws IOException {
+
+        if (!Files.exists(FILE_PATH)) {
+            InitializationFile();
+        }
+
+        List<String> lines = Files.readAllLines(FILE_PATH);
+
+        for (String line : lines) {
+            System.out.println(line);
         }
     }
 }
